5f9b95f3048cafe904fb9cc434059a75db54ea7d
Zdw <daiwei@yahaha.com>
2024-04-26 17:37:32 +0800

    fix eventbus

--- locate/redis/locator.go
+++ locate/redis/locator.go
@@ -52,9 +52,9 @@ func NewLocator(opts ...Option) *Locator {
 
 		statusCmd := o.client.Ping(o.ctx)
 		if statusCmd.Err() != nil {
-			log.Errorf("redis[%+v] start failed. statusCmd:%+v", o.addr, statusCmd)
+			log.Errorf("[locate] redis[%+v] start failed. statusCmd:%+v", o.addr, statusCmd)
 		} else {
-			log.Infof("redis[%+v] start success.", o.addr)
+			log.Infof("[locate] redis[%+v] start success.", o.addr)
 		}
 	}
 
